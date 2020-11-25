28b93d39b9934ff32fc48b120f1a268e5ffe3ccf
sarisgar28 <sarisgar28@gmail.com>
2020-11-25 12:48:21 -0500

    landmarks

--- app/controllers/landmark_controller.rb
+++ app/controllers/landmark_controller.rb
@@ -1,7 +1,43 @@
 class LandmarkController < ApplicationController
+    before_action :find_landmark, only: [:show, :edit, :update, :destroy]
 
     def index 
         @landmarks = Landmark.all
     end 
+
+
+    def new 
+        @landmark = Landmark.new 
+    end 
+
+
+    def create
+        @landmark = Landmark.new(landmark_params)
+        if @landmark.save
+            redirect_to landmark_path(@landmark)
+        else 
+            render :new 
+        end 
+    end 
+
+    def update 
+        @landmark.update(landmark_params)
+        redirect_to landmark_path(@landmark)
+    end 
+
+    def destroy 
+        @landmark.destroy
+        redirect_to landmark_path 
+    end 
+
+private
+
+def find_landmark
+    @landmark = Landmark.find_by(id: params[:id])
+end 
+
+def landmark_params
+    params.require(:landmark).permit(:name, :city, :country, :description, :user_id, :review_id)
+end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :sessions
-  resources :landmarks
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/landmarks', to: 'landmark#index'
 end
