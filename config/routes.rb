656ed30cb975011a605e451c30b4ad0378f40ab3
sarisgar28 <sarisgar28@gmail.com>
2020-11-26 12:03:08 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  get '/landmarks', to: 'landmark#index'
+#new 
+  get "/landmarks/new", to: "landmarks#new", as: "new_landmark_path"
+
+#create 
+post "/landmarks/create", to: "landmarks#create", as: "landmarks_path"
+#index
+  get "/landmarks", to: "landmarks#index"
+
+#edit
+get "/landmarks/:id/edit", to: "landmarks#edit", as: "edit_landmark_path"
+
+#show
+get "/landmarks/:id", to: "landmarks#show", as: "landmark_path"
+
+#update
+patch "landmarks/:id", to: "landmarks#update"
+
+#destroy 
+delete "landmarks/:id", to: "landmarks#destroy", as: "destroy_landmark_path"
+
   resources :landmarks
 end
