6c73319387eea38fe56245eb23c751e5e9136cc6
sarisgar28 <sarisgar28@gmail.com>
2020-12-01 09:08:33 -0500

    landmark's reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,15 @@ class ReviewsController < ApplicationController
         @landmark = Landmark.new
         @review = Landmark.reviews.new 
     end 
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            flash[:message] = "Review has been added for #{@review.landmark.name}"
+        redirect_to landmark_path(@review.landmark)
+        else
+            render :new 
+        end 
+    end 
+
 end
