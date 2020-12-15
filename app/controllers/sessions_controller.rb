627d2a4f2e9f96d15983ef39731c429b1639790b
sarisgar28 <sarisgar28@gmail.com>
2020-12-14 19:09:45 -0500

    css

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
     
       def logout
         session.clear
-        redirect_to login_path
+        redirect_to root_path
       end
 
       def google_login
